072d866a5534e606083fb3cd2a10cf1f9726f714
Eppelflepp <oliviermorssink@gmail.com>
2023-07-16 10:49:28 +0200

    Changed the follow message to fix grammer mistakes

--- pages/api/follow.ts
+++ pages/api/follow.ts
@@ -38,7 +38,7 @@ export default async function handler(
             try {
                 await prisma.notification.create({
                     data: {
-                        body: `@${currentUser?.username} started followed you!`,
+                        body: `@${currentUser?.username} started following you!`,
                         userId
                     }
                 });
