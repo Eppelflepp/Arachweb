6870a65c61f050990cbf851e474c0423dd70c87e
Eppelflepp <oliviermorssink@gmail.com>
2023-07-25 08:23:35 +0200

    Fixing building error

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,3 +1,5 @@
+// Form.tsx
+
 import axios from 'axios';
 import { useCallback, useState } from 'react';
 import { toast } from 'react-hot-toast';
@@ -22,7 +24,7 @@ const Form: React.FC<FormProps> = ({ placeholder, isComment, postId }) => {
   const loginModal = useLoginModal();
 
   const { data: currentUser } = useCurrentUser();
-  const { mutate: mutatePosts } = usePosts();
+  const { mutate: mutatePosts } = usePosts(currentUser?.id); // Pass currentUser?.id as the userId argument if available
   const { mutate: mutatePost } = usePost(postId as string);
 
   const [body, setBody] = useState('');
@@ -47,56 +49,52 @@ const Form: React.FC<FormProps> = ({ placeholder, isComment, postId }) => {
     }
   }, [body, mutatePosts, isComment, postId, mutatePost]);
 
+  // If user is not logged in, show the login/register options
+  if (!currentUser || !currentUser.id) {
+    return (
+      <div className="py-8">
+        <h1 className="text-white text-2xl text-center mb-4 font-bold">Welcome to Twitter</h1>
+        <div className="flex flex-row items-center justify-center gap-4">
+          <Button label="Login" onClick={loginModal.onOpen} />
+          <Button label="Register" onClick={registerModal.onOpen} secondary />
+        </div>
+      </div>
+    );
+  }
+
+  // If user is logged in, show the tweet form
   return (
     <div className="border-b-[1px] border-neutral-800 px-5 py-2">
-      {currentUser ? (
-        <div className="flex flex-row gap-4">
-          <div>
-            <Avatar userId={currentUser?.id} />
-          </div>
-          <div className="w-full">
-            <textarea
-              disabled={isLoading}
-              onChange={(event) => setBody(event.target.value)}
-              value={body}
-              className="
-                disabled:opacity-80
-                peer
-                resize-none 
-                mt-3 
-                w-full 
-                bg-black 
-                ring-0 
-                outline-none 
-                text-[20px] 
-                placeholder-neutral-500 
-                text-white
-              "
-              placeholder={placeholder}>
-            </textarea>
-            <hr 
-              className="
-                opacity-0 
-                peer-focus:opacity-100 
-                h-[1px] 
-                w-full 
-                border-neutral-800 
-                transition"
-            />
-            <div className="mt-4 flex flex-row justify-end">
-              <Button disabled={isLoading || !body} onClick={onSubmit} label="Tweet" />
-            </div>
+      <div className="flex flex-row gap-4">
+        <div>
+          <Avatar userId={currentUser.id} />
+        </div>
+        <div className="w-full">
+          <textarea
+            disabled={isLoading}
+            onChange={(event) => setBody(event.target.value)}
+            value={body}
+            className="
+              disabled:opacity-80
+              peer
+              resize-none 
+              mt-3 
+              w-full 
+              bg-black 
+              ring-0 
+              outline-none 
+              text-[20px] 
+              placeholder-neutral-500 
+              text-white
+            "
+            placeholder={placeholder}
+          />
+          <hr className="opacity-0 peer-focus:opacity-100 h-[1px] w-full border-neutral-800 transition" />
+          <div className="mt-4 flex flex-row justify-end">
+            <Button disabled={isLoading || !body} onClick={onSubmit} label="Tweet" />
           </div>
         </div>
-      ) : (
-        <div className="py-8">
-          <h1 className="text-white text-2xl text-center mb-4 font-bold">Welcome to Twitter</h1>
-          <div className="flex flex-row items-center justify-center gap-4">
-            <Button label="Login" onClick={loginModal.onOpen} />
-            <Button label="Register" onClick={registerModal.onOpen} secondary />
-          </div>
-        </div>
-      )}
+      </div>
     </div>
   );
 };
